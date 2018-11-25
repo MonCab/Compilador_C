00032aa60318a0fba19271062b65ea808052864e
gerry0509 <39445714+gerry0509@users.noreply.github.com>
2018-11-24 20:16:47 -0600

    Update tablaTipo.c

--- tablaTipo.c
+++ tablaTipo.c
@@ -1,24 +1,40 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
-typedef struct NodoL{
+typedef struct NodoA{
 	char tipo[100];
-	struct Nodo *next;
+	struct NodoA *next;
 }TipoArgs;
 typedef TipoArgs *ANodo;
 typedef TipoArgs *ALista;
-typedef struct Nodo{
+typedef struct NodoL{
 	int pos;
 	char id[100];
 	int tipo;
 	int dir;
 	char var[20];
-	Lista args;
+	ALista args;
+	struct NodoL *next;
 }TipoNodoL;
 typedef TipoNodoL *TNodo;
 typedef TipoNodoL *TLista;
-void insertArg(Lista *lista,char *arg);
+void insertArg(ALista *Lista,char *arg);
 
 int main(){
 	
 	return 0;
+}
+void insertArg(ALista *Lista,char *arg){
+	ANodo nuevo,anterior;
+	nuevo=(ANodo)malloc(sizeof(TipoArgs));
+	strcpy(nuevo->tipo,arg);
+	if(*Lista==NULL){		
+		nuevo->next=*Lista;
+		*Lista=nuevo;
+	}else{
+		anterior=*Lista;	
+		while(anterior->next) anterior=anterior->next;
+		nuevo->next=anterior->next;
+		anterior->next=nuevo;	
+	}
 }
