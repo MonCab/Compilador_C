b1e423c912a7a40eabea16ff262f14170f497ad7
gerry0509 <39445714+gerry0509@users.noreply.github.com>
2018-11-24 21:24:57 -0600

    Update tablaTipo.c

--- tablaTipo.c
+++ tablaTipo.c
@@ -18,9 +18,19 @@ typedef struct NodoS{
 }TipoSimbol;
 typedef TipoSimbol *SNodo;
 typedef TipoSimbol *SLista;
+typedef struct NodoT{
+	int pos;
+	char tipo[50];
+	int tam;
+	int dim;
+	int tipoBase;
+	struct NodoT *next;
+}TipoTipo;
+typedef TipoTipo *TNodo;
+typedef TipoTipo *TLista;
 void insertArg(ALista *Lista,char *arg);
 void insertSimbol(SLista *Lista,int pos,char *id,int tipo,int dir,char *var,ALista *args);
-
+void insertTipo(TLista *Lista,int pos,char *tipo,int tam, int dim, int tipoBase);
 int main(){
 	
 	return 0;
@@ -49,6 +59,24 @@ void insertSimbol(SLista *Lista,int pos,char *id,int tipo,int dir,char *var,ALis
 	nuevo->dir=dir;
 	strcpy(nuevo->var,var);
 	nuevo->args=args;
+	if(*Lista==NULL){		
+		nuevo->next=*Lista;
+		*Lista=nuevo;
+	}else{
+		anterior=*Lista;	
+		while(anterior->next) anterior=anterior->next;
+		nuevo->next=anterior->next;
+		anterior->next=nuevo;	
+	}
+}
+void insertTipo(TLista *Lista,int pos,char *tipo,int tam, int dim, int tipoBase){
+	TNodo nuevo,anterior;
+	nuevo=(TNodo)malloc(sizeof(TipoTipo));
+	nuevo->pos=pos;
+	strcpy(nuevo->tipo,tipo);
+	nuevo->tam=tam;
+	nuevo->dim=dim;
+	nuevo->tipoBase=tipoBase;
 	if(*Lista==NULL){		
 		nuevo->next=*Lista;
 		*Lista=nuevo;
